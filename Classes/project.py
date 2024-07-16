c82582c3d0e954c36318122b6531d29a94a24f5c
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-16 15:51:16 +0200

    Create project.py
    Creating the Project Class

---
+++ Classes/project.py
@@ -0,0 +1,6 @@
+class Project:
+    def __init__(self, id, name, description, tasks=None):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.tasks = tasks if tasks is not None else []
