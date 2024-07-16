7754a36d3e562cceba3d086f1c13c2997ecfded3
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-16 16:23:47 +0200

    Update project.py
    Changed the task parameter to task_id to match the logic of the method that uses a task ID for deletion.

--- Classes/project.py
+++ Classes/project.py
@@ -3,18 +3,18 @@ class Project:
         self.id = id
         self.name = name
         self.description = description
-        self.tasks = tasks if tasks is not None else [] #List of tasks associated with the project
-        
+        self.tasks = tasks if tasks is not None else []  # List of tasks associated with the project
+
     def add_task(self, task):
-        self.tasks.append(task)
-        
-    def remove_task(self, task):
-        self.tasks = [task for task in self.tasks if task.id != task.id] #Creates a new task list that does not contain the task with the ID task_id
+        self.tasks.append(task)  # Add a task to the project
+
+    def remove_task(self, task_id):
+        self.tasks = [task for task in self.tasks if task.id != task_id]  # Create a new task list without the task with ID task_id
         
     def get_task(self, task_id):
         for task in self.tasks:
             if task.id == task_id:
-                return task  #Goes through all the tasks and returns the one with the ID that matches task_id.
-
+                return task  # Go through all the tasks and return the one that matches the ID task_id
+            
     def __str__(self):
-                return f"Project({self.id}, '{self.name}', {len(self.tasks)} Tasks)"
+        return f"Project({self.id}, '{self.name}', {len(self.tasks)} Tasks)"
