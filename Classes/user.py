6047755a03ed24cff46ddb23bf89df0a48593cf5
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-16 17:41:51 +0200

    Update user.py
    Added method to display the user's ID, email and nickname

--- Classes/user.py
+++ Classes/user.py
@@ -6,3 +6,6 @@ class User:
         
     def assign_task(self, task):
         task.assigned_to = self.id # Assigns a task to the user
+        
+    def __str__(self):
+        return f"User({self.id}, '{self.username}', '{self.email}')"
